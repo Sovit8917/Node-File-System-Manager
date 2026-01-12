c8c8d082aa5b52c1bcc7ecfe60392f1203a278fc
Sovita Sarbeswar Lenka <lenkasovitsarbeswar@gmail.com>
2026-01-12 19:42:39 +0530

    Delete inbuilt/first directory

--- inbuilt/first/app.js
+++
@@ -1,7 +0,0 @@
-// import os from 'node:os'
-// console.log("Total Memory",os.totalmem());
-// console.log("Memory",os.freemem());
-// console.log("Hostname",os.hostname());
-// console.log("userinfo",os.userInfo());
-// console.log("Machine",os.machine());
-
